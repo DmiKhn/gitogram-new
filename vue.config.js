2d440c11cee59f1262ad38ddb2bfdb4204e63fe3
Dmitry Khanenya <dmikhn.work@gmail.com>
2023-08-23 23:52:24 +0300

    fifth push

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/Gitogram/dist/',
+  publicPath: '/gitogram-new/dist/',
   transpileDependencies: true
 })
