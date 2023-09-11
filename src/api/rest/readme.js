943b618e37ba4257d5d4eb7b261ffbc65e5cbf52
Dmitry Khanenya <dmikhn.work@gmail.com>
2023-09-11 14:03:21 +0300

    readme problems

--- src/api/rest/readme.js
+++ src/api/rest/readme.js
@@ -1,7 +1,7 @@
-import { makeRequest } from '@/api/requests';
+import { makeRequest } from '../requests'
 
 export const getReadme = ({ owner, repo }) => {
-  const contentHeader = 'application/vnd.github.v3html+json'
+  const contentHeader = 'application/vnd.github.v3.html+json'
   return makeRequest({
     url: `/repos/${owner}/${repo}/readme`,
     headers: {
